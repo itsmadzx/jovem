a48f772f302f8f979702823c628d8ead53214961
itsmadzx <eduardamaria301208@gmail.com>
2026-09-07 22:02:46 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'jovem',
+  webDir: 'www'
+};
+
+export default config;
